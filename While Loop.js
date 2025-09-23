8209322724b764a77c2080048e0e36d8ec4a2aab
Brandon <brandonopere6@gmail.com>
2025-09-23 21:25:18 +0300

    while loops

---
+++ While Loop.js
@@ -0,0 +1,6 @@
+let candy = 3;
+
+while (candy > 0) {
+    console.log("Eat candy");
+    candy--;  // take 1 candy away
+}
---
+++ While Loop.py
@@ -0,0 +1,5 @@
+candy = 3
+
+while candy > 0:
+    print("Eat candy")
+    candy -= 1   # take 1 candy away
